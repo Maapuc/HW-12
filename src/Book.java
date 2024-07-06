e792800b782e57f98de19435d4317b8d890afecc
Nikolai <pojarka754@gmail.com>
2024-07-06 13:19:28 +0500

    Init

--- src/Book.java
+++ src/Book.java
@@ -3,4 +3,26 @@ public class Book {
     private Author author;
     private int publisherYear;
 
+    public Book(String title, int publisherYear, Author author) {
+        this.title = title;
+        this.publisherYear = publisherYear;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublisherYear() {
+        return publisherYear;
+    }
+
+    public void setPublisherYear(int publisherYear) {
+        this.publisherYear = publisherYear;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
 
+        Author nikolai = new Author("Николай", "Дроздов");
+        Book tom1 = new Book("В мире животных", 1960, nikolai);
+        Author kyk = new Author("Кук", "Джейис");
+        Book adventure = new Book("Канибалы", 1750, kyk);
+        System.out.println(adventure.getPublisherYear());
+        adventure.setPublisherYear(1779);
+        System.out.println(adventure.getPublisherYear());
     }
+
+}
